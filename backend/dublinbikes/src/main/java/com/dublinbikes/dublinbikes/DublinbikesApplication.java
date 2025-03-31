e47e94f2b35f31f752dfa9dfad23e947bf3d5b29
CGebben <coltontgebben@gmail.com>
2025-03-31 19:47:30 +0100

    Initialize Spring Boot backend and Configure PostgreSQL with Docker
    - Generated Spring Boot project with dependencies: Spring Web, Spring Data JPA, PostgreSQL Driver, Lombok, and DevTools.
    - Added Docker Compose configuration for running PostgreSQL locally. Included .env file for sensitive DB credentials (excluded from Git tracking).
    - Confirmed PostgreSQL container runs successfully and accepts connections.

---
+++ backend/dublinbikes/src/main/java/com/dublinbikes/dublinbikes/DublinbikesApplication.java
@@ -0,0 +1,13 @@
+package com.dublinbikes.dublinbikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DublinbikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DublinbikesApplication.class, args);
+	}
+
+}
---
+++ backend/dublinbikes/src/test/java/com/dublinbikes/dublinbikes/DublinbikesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dublinbikes.dublinbikes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DublinbikesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
