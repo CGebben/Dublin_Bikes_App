599cb63f09cddc30fbe1b75ded6461917a18636a
CGebben <coltontgebben@gmail.com>
2025-06-18 12:43:14 -0500

    Updated frontend and CorsConfig to support GitHub Pages deployment

--- dublin-bikes-frontend/vite.config.js
+++ dublin-bikes-frontend/vite.config.js
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vite.dev/config/    local deployment
+// export default defineConfig({
+  // plugins: [react()],
+  // server: {
+    // proxy: {
+      // '/stations': 'http://localhost:8080',
+      // '/availability': 'http://localhost:8080',
+      // '/weather': 'http://localhost:8080',
+    // },
+  // },
+// });
+
+// For Github pages deployment.
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/stations': 'http://localhost:8080',
-      '/availability': 'http://localhost:8080',
-      '/weather': 'http://localhost:8080',
-    },
-  },
+  base: '/Dublin_Bikes_App/',
 });
